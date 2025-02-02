d23eb6b9997b25e97471a084e908354eaf081245
varshitha m <jnvvarshu@gmail.com>
2025-02-02 13:34:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GradeDash from './components/GradeDashboard/GradeDash';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <GradeDash />
+    </div>
+  );
+}
+
+export default App;
